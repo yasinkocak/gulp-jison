338c95f326e4bfc873813937bf95efec69e74191
Ger Hobbelt <ger@hobbelt.com>
2015-02-11 23:58:28 +0100

    - making the gulp plugin work with jison 0.4.15 (and properly include the generated lexer code)
    - adding three callbacks for pre/postprocessing the grammar file and the generated output

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
 var through = require('through2');
 var gutil = require('gulp-util');
+var objectAssign = require('object-assign');
 var PluginError = gutil.PluginError;
-var Parser = require('jison').Parser;
+var jison = require('jison');
 
 const PLUGIN_NAME = 'gulp-jison';
 
 module.exports = function (options) {
     options = options || {};
 
+    // always produce a working function
+    function mkF(f, default_f) {
+        if (typeof f !== 'function') {
+            return default_f;
+        }
+        return f;
+    }
+
     return through.obj(function (file, enc, callback) {
         if (file.isNull()) {
-            this.push(file);
-            return callback();
+            callback(null, file);
+            return;
         }
 
         if (file.isStream()) {
             this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported'));
-            return callback();
+            callback();
+            return;
         }
 
         if (file.isBuffer()) {
+            var fileOpts = objectAssign({}, options);
+            
+            // special callbacks:
+            var preprocessor = mkF(fileOpts.preprocessor, function (file, content, options) {
+                return content;
+            });
+            var postprocessor = mkF(fileOpts.postprocessor, function (file, content, options) {
+                return content;
+            });
+            var customizer = mkF(fileOpts.customizer, function (file, options) {
+                console.log("jison file: ", file, options);
+            });
+            
+            // do not pollute the Jison environment with our own options:
+            delete fileOpts.preprocessor;
+            delete fileOpts.postprocessor;
+            delete fileOpts.customizer;
+
             try {
-                file.contents = new Buffer(new Parser(file.contents.toString(), options).generate());
+                customizer(file, fileOpts);
+                var source_contents = file.contents.toString();
+                source_contents = preprocessor(file, source_contents, fileOpts);
+
+                var gen = jison.Generator(source_contents, fileOpts);
+                var dest_contents = gen.generate();
+
+                dest_contents = postprocessor(file, dest_contents, fileOpts);
+                file.contents = new Buffer(dest_contents);
+                
                 file.path = gutil.replaceExtension(file.path, ".js");
                 this.push(file);
             } catch (error) {
                 this.emit('error', new PluginError(PLUGIN_NAME, error));
             }
-            return callback();
+            callback();
         }
     });
 }
